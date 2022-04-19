39cca5f5f9aca262a457af059c3cdf2b5e0775ad
Skolaczk <skolakmichal1@gmail.com>
2022-04-19 11:56:23 +0200

    Add initial files
    Create a new app with CRA. Install styled-components.
    Create theme and global styles files. Install json-server
    as a devDependencie and create database file. Configure
    tools for better wrinting: prettier, eslint, husky
    and lint-staged.

---
+++ src/assets/styles/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'Lato', 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    font-size: 1.6rem;
+  }
+`;
---
+++ src/assets/styles/theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    black: '#000000',
+    blue: '#0082FA',
+    white: '#FFFFFF',
+    darkGray: '#1A1A1A',
+    gray: '#4D4D4D',
+    lightGray: '#707070',
+  },
+  font: {
+    weight: {
+      regular: 400,
+      bold: 7000,
+    },
+    size: {
+      xs: '13px',
+      s: '14px',
+      m: '20px',
+      l: '26px',
+      xl: '37px',
+    },
+  },
+  mq: {
+    tablet: '@media (min-width: 768px)',
+    desktop: '@media (min-width: 1024px)',
+    bigDesktop: '@media (min-width: 1280px)',
+    huge: '@media(min-width: 1440px)',
+  },
+};
